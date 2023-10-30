f622eb0caaa744ed68cef11251a74978e33dc5ea
Peklerfix <peklerfix@gmail.com>
2023-10-30 22:14:16 +0100

    SeedUserData has been excluded from SeedData  from duende is project and moved t oAPI, which is responsible for handling user-related opeprations

--- PerfumeStore.API/DuendeIs/SeedData.cs
+++ PerfumeStore.API/DuendeIs/SeedData.cs
@@ -20,7 +20,7 @@ namespace DuendeIs
         var context = scope.ServiceProvider.GetService<ConfigurationDbContext>();
         context.Database.Migrate();
         EnsureSeedData(context);
-        EnsureUsers(scope);
+        //EnsureUsers(scope);
       }
     }
 
@@ -101,79 +101,6 @@ namespace DuendeIs
       else
       {
         Log.Debug("OIDC IdentityProviders already populated");
-      }
-    }
-
-    private static void EnsureUsers(IServiceScope scope)
-    {
-      var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-      var alice = userMgr.FindByNameAsync("alice").Result;
-      if (alice == null)
-      {
-        alice = new IdentityUser
-        {
-          UserName = "alice",
-          Email = "AliceSmith@email.com",
-          EmailConfirmed = true,
-        };
-        var result = userMgr.CreateAsync(alice, "Pass123$").Result;
-        if (!result.Succeeded)
-        {
-          throw new Exception(result.Errors.First().Description);
-        }
-
-        result = userMgr.AddClaimsAsync(alice, new Claim[]
-        {
-        new Claim(JwtClaimTypes.Name, "Alice Smith"),
-        new Claim(JwtClaimTypes.GivenName, "Alice"),
-        new Claim(JwtClaimTypes.FamilyName, "Smith"),
-        new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
-        }).Result;
-        if (!result.Succeeded)
-        {
-          throw new Exception(result.Errors.First().Description);
-        }
-
-        Log.Debug("alice created");
-      }
-      else
-      {
-        Log.Debug("alice already exists");
-      }
-
-      var bob = userMgr.FindByNameAsync("bob").Result;
-      if (bob == null)
-      {
-        bob = new IdentityUser
-        {
-          UserName = "bob",
-          Email = "BobSmith@email.com",
-          EmailConfirmed = true
-        };
-        var result = userMgr.CreateAsync(bob, "Pass123$").Result;
-        if (!result.Succeeded)
-        {
-          throw new Exception(result.Errors.First().Description);
-        }
-
-        result = userMgr.AddClaimsAsync(bob, new Claim[]
-        {
-        new Claim(JwtClaimTypes.Name, "Bob Smith"),
-        new Claim(JwtClaimTypes.GivenName, "Bob"),
-        new Claim(JwtClaimTypes.FamilyName, "Smith"),
-        new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
-        new Claim("location", "somewhere")
-        }).Result;
-        if (!result.Succeeded)
-        {
-          throw new Exception(result.Errors.First().Description);
-        }
-
-        Log.Debug("bob created");
-      }
-      else
-      {
-        Log.Debug("bob already exists");
       }
     }
   }
---
+++ PerfumeStore.API/PerfumeStore.API/SeedUserData.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Claims;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using Serilog;
+
+namespace PerfumeStore.API
+{
+
+  public class SeedUserData
+  {
+    public static void EnsureUsers(IServiceProvider serviceProvider)
+    {
+      using (var scope = serviceProvider.CreateScope())
+      {
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+        // Dodaj użytkownika Alice, jeśli nie istnieje
+        var alice = userManager.FindByNameAsync("alice").Result;
+        if (alice == null)
+        {
+          alice = new IdentityUser
+          {
+            UserName = "alice",
+            Email = "AliceSmith@email.com",
+            EmailConfirmed = true,
+          };
+          var result = userManager.CreateAsync(alice, "Pass123$").Result;
+          if (!result.Succeeded)
+          {
+            throw new Exception(result.Errors.First().Description);
+          }
+        }
+
+        // Dodaj użytkownika Bob, jeśli nie istnieje
+        var bob = userManager.FindByNameAsync("bob").Result;
+        if (bob == null)
+        {
+          bob = new IdentityUser
+          {
+            UserName = "bob",
+            Email = "BobSmith@email.com",
+            EmailConfirmed = true,
+          };
+          var result = userManager.CreateAsync(bob, "Pass123$").Result;
+          if (!result.Succeeded)
+          {
+            throw new Exception(result.Errors.First().Description);
+          }
+        }
+      }
+    }
+  }
+}
