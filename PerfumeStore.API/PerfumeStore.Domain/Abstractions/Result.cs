7325c2173523e4173f38a6eb4e0875f30411eeeb
Peklerfix <peklerfix@gmail.com>
2023-11-20 23:37:03 +0100

    Error, Result and ProductErrors classes have been adeed for handling error, not implemented in code yet

---
+++ PerfumeStore.API/PerfumeStore.Domain/Abstractions/Result.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfumeStore.Domain.Abstractions
+{
+    public class Result
+    {
+        private Result(bool isSuccess, Error error)
+        {
+            if (isSuccess && error != Error.None || 
+                !isSuccess && error == Error.None)
+            {
+                throw new ArgumentException("Invalid error", nameof(error));
+            }
+
+            isSuccess = isSuccess;
+            Error = error;
+        }
+
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public Error Error { get; }
+
+        public static Result Success() => new(true, Error.None);
+        public static Result Failure(Error error) => new(false, error);
+    }
+}
