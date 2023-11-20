28077515d5a9a5bd506c07a6f25c98905ff3e7ee
Krzysztof Kozłowski <106349197+Ksztof@users.noreply.github.com>
2023-11-21 00:02:53 +0100

    Error, Result and ProductErrors classes have been adeed for handling error, not implemented in code yet (#93)
    Co-authored-by: Peklerfix <peklerfix@gmail.com>

---
+++ PerfumeStore.API/PerfumeStore.Domain/Abstractions/Error.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfumeStore.Domain.Abstractions
+{
+    public sealed record Error(string Code, string? Description = null)
+    {
+        public static readonly Error None = new(string.Empty);
+
+        public static implicit operator Result(Error error) => Result.Failure(error);
+    }
+}
