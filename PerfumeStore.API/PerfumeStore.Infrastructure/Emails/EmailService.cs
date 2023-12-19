3da1552b53faa33d1675d3771b67f6b161608af9
Peklerfix <peklerfix@gmail.com>
2023-12-19 16:10:13 +0100

    Sending  email with order has been done

--- PerfumeStore.API/PerfumeStore.Infrastructure/Emails/EmailService.cs
+++ PerfumeStore.API/PerfumeStore.Infrastructure/Emails/EmailService.cs
@@ -22,9 +22,9 @@ namespace PerfumeStore.Infrastructure.Emails
 
         public EmailService(
             IEmailSender emailSender,
-            IUrlHelper urlHelper, 
+            IUrlHelper urlHelper,
             IHttpContextService httpContextService
-            ) 
+            )
         {
             _emailSender = emailSender;
             _urlHelper = urlHelper;
@@ -44,8 +44,9 @@ namespace PerfumeStore.Infrastructure.Emails
             string message = $@"
               <h2>Hello {userDetails.UserName},</h2>
               <p>We invite you to start using our service.</p>
-Thank you for shopping
-              <a href='{confirmationLink}' style='display: inline-block; padding: 10px 20px; background-color: #3498db; color: white; text-decoration: none; border-radius: 5px; margin-top: 20px;'>Activate Account</a>
+                 Thank you for shopping
+              <a href='{confirmationLink}' style='display: inline-block; padding: 10px 20px; background-color: #3498db; color:
+                white; text-decoration: none; border-radius: 5px; margin-top: 20px;'>Activate Account</a>
               <p style='margin-top: 20px;'>Instead, you can copy/paste this link into your browser:</p>
               <div style='background-color: #f3f3f3; padding: 10px; border-radius: 5px;'>
                   <a href='{confirmationLink}' style='color: #3498db; text-decoration: none;'>{confirmationLink}</a>
@@ -58,57 +59,48 @@ Thank you for shopping
             string subject = "Order Summary";
             int rowIndex = 0;
             string message = $@"
-                <h2>Hello {orderResponse.ShippingDetil.FirstName}, here is  your order summary, thank you for shopping!</h2>
-
-                <table>
-                    <tr style='background-color: grey;'>
-                        <th colspan='4'>ORDER SUMMARY</th>
-                    </tr>
-                    <tr>
-                        <th>Product name</th>
-                        <th>Product unit price</th>
-                        <th>Product quantity</th>
-                        <th>Product total price</th>  
-                    </tr>
-            ";
-
+                <html>
+                    <body style='font-family: Arial, sans-serif; font-size: 16px; background-color: #f4f4f4; margin: 0; padding: 20px;'>
+                        <div style='text-align: center; margin-bottom: 20px;'>
+                            <h2 style='color: #333;'>Hello {orderResponse.ShippingDetil.FirstName}, here is your order summary, thank you for shopping!</h2>
+                        </div>
+                        <div style='background: #fff; border-radius: 10px; overflow: hidden; box-shadow: 0 4px 8px rgba(0,0,0,0.1);'>
+                            <table style='width: 100%; border-collapse: collapse; margin: 0; font-size: 0.9em;'>
+                                <thead>
+                                    <tr style='background-color: #7B68EE; color: #ffffff; text-align: center;'>
+                                        <th style='padding: 15px;'>Product name</th>
+                                        <th style='padding: 15px;'>Product unit price</th>
+                                        <th style='padding: 15px;'>Product quantity</th>
+                                        <th style='padding: 15px;'>Product total price</th>  
+                                    </tr>
+                                </thead>
+                                <tbody>";
             foreach (CheckCartDto product in orderResponse.AboutProductsInCart)
             {
-                if (rowIndex % 2 == 0)
-                {
-                    message += $@"
-                        <tr>
-                            <td>PRODUCT NAME</td>
-                            <td>{product.ProductUnitPrice}</td>
-                            <td>{product.Quantity}</td>
-                            <td>{product.ProductUnitPrice} X {product.Quantity} = {product.ProductTotalPrice}</td>
-                        </tr>
-                    ";
-                }
-                else
-                {
-                    message += $@"
-                        <tr style='background-color: gray;'>
-                            <td>PRODUCT NAME</td>
-                            <td>{product.ProductUnitPrice}</td>
-                            <td>{product.Quantity}</td>
-                            <td>{product.ProductUnitPrice} X {product.Quantity} = {product.ProductTotalPrice}</td>
-                        </tr>
-                    ";
-                }
-
+                string backgroundColor = rowIndex % 2 == 0 ? "#ffffff" : "#f2f2f2";
+                message += $@"
+                                    <tr style='background-color: {backgroundColor};'>
+                                        <td style='padding: 10px; text-align: center;'>{product.ProductName}</td>
+                                        <td style='padding: 10px; text-align: center;'>{product.ProductUnitPrice:C}</td>
+                                        <td style='padding: 10px; text-align: center;'>{product.Quantity}</td>
+                                        <td style='padding: 10px; text-align: center;'>{product.ProductUnitPrice:C} X {product.Quantity} szt. = {product.ProductTotalPrice:C}</td>
+                                    </tr>";
                 rowIndex++;
             }
             message += $@"
-                <tr><td colspan='4'><strong>Shipping details</strong></td></tr>
-                <tr><td>First name:</td><td>{orderResponse.ShippingDetil.FirstName}</td></tr>
-                <tr><td>Last name:</td><td>{orderResponse.ShippingDetil.LastName}</td></tr>
-                <tr><td>Email:</td><td>{orderResponse.ShippingDetil.Email}</td></tr>
-                <tr><td>Phone:</td><td>{orderResponse.ShippingDetil.PhoneNumber}</td></tr>
-                <tr><td>Address:</td><td>{orderResponse.ShippingDetil.Street} {orderResponse.ShippingDetil.StreetNumber}{(string.IsNullOrEmpty(orderResponse.ShippingDetil.HomeNumber) ? "" : "/" + orderResponse.ShippingDetil.HomeNumber)}, {orderResponse.ShippingDetil.PostCode}, {orderResponse.ShippingDetil.City}</td></tr>
-            ";
-
-            message += "</table>";
+                                </tbody>
+                            </table>
+                        </div>
+                        <div style='margin-top: 20px; padding: 15px; background: #fff; border-radius: 10px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);'>
+                            <h4 style='color: #333;'>Shipping details</h4>
+                            <p style='color: #666;'>First name: {orderResponse.ShippingDetil.FirstName}</p>
+                            <p style='color: #666;'>Last name: {orderResponse.ShippingDetil.LastName}</p>
+                            <p style='color: #666;'>Email: {orderResponse.ShippingDetil.Email}</p>
+                            <p style='color: #666;'>Phone: {orderResponse.ShippingDetil.PhoneNumber}</p>
+                            <p style='color: #666;'>Address: {orderResponse.ShippingDetil.Street} {orderResponse.ShippingDetil.StreetNumber}{(string.IsNullOrEmpty(orderResponse.ShippingDetil.HomeNumber) ? "" : " " + orderResponse.ShippingDetil.HomeNumber)}, {orderResponse.ShippingDetil.PostCode}, {orderResponse.ShippingDetil.City}</p>
+                        </ div >
+                    </ body >
+                </ html > ";
 
             await _emailSender.SendEmailAsync(orderResponse.ShippingDetil.Email, subject, message);
         }
