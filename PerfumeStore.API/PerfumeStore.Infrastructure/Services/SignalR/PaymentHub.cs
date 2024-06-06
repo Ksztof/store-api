860535b1349918ac231ecbdde9a6ec08072379a5
Krzysztof Kozłowski <106349197+Ksztof@users.noreply.github.com>
2024-06-06 21:07:10 +0100

    Stripe/payments (#161)
    * CORS configuration
    
    * CORS works
    
    * Authentication for JWT Cookie works
    
    * public DateTime CreatedAt { get; set; }
    filed has been created in Cart.cs entity this field is used for cart serialization
    
    * AboutCartDto now returns ifnormation about cart creation date
    
    * CheckCart function from cart controller now return NoContent when cart is empty
    
    * IsCurrentCartAsync function has been added
    
    * cart CreatedAt field is updated with new date when changes in cart occur
    
    * temp commit
    
    * CheckCart funstion returns always CreatedAt value as UTC
    
    * fixed typo in CheckCurrentCart validator
    
    * PaymentsController and PaymentsService have been added, stripe has been configured in program.cs, PayWithCardAsync for card payment has been created
    
    * VerifyPaymentAsync in payements service and   StripeWebhook in payemments controller have been created, webhook secret has bee nadded
    
    * function PayWithCardAsync now saves in stripe metadata cartId, function VerifyPaymentAsync has access to cart id from metadata Result pattern named Result has been creatted for common errors
    
    * VerifyPaymentAsync method is done
    
    * VerifyPaymentAsync has been upgraded
    
    * public async Task VerifyPaymentAsync()
    is using signal R to communicate with frontend
    
    * CORS policy has been extended of "https://dashboard.stripe.com",
    "https://hooks.stripe.com"
    
    * PayWithCardAsync and VerifyPaymentAsync are using now OrderId instead of CartId to verify order
    
    * Update .gitignore to track .pfx file
    
    * stripe web hook can be started by stripe api, Perfume store API works now on HTTPS
    
    * VerifyPaymentAsync has been updated
    
    * Communication with frontend during payment process via signalR works properly
    
    ---------
    
    Co-authored-by: Peklerfix <peklerfix@gmail.com>

--- PerfumeStore.API/PerfumeStore.Infrastructure/Services/SignalR/PaymentHub.cs
+++ PerfumeStore.API/PerfumeStore.Infrastructure/Services/SignalR/PaymentHub.cs
@@ -9,20 +9,21 @@ using System.Threading.Tasks;
 namespace PerfumeStore.Infrastructure.Services.SignalR
 {
     public class PaymentHub : Hub
+{
+    public async Task JoinGroup(string orderId)
     {
-        public async Task JoinGroup(string orderId)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, orderId);
-        }
-
-        public async Task LeaveGroup(string orderId)
-        {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, orderId);
-        }
-
-        public async Task SendPaymentStatus(string orderId, string status)
-        {
-            await Clients.Group(orderId).SendAsync("ReceivePaymentStatus", status);
-        }
+        await Groups.AddToGroupAsync(Context.ConnectionId, orderId);
     }
+
+    public async Task LeaveGroup(string orderId)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, orderId);
+    }
+
+    public async Task SendPaymentStatus(string orderId, string status)
+    {
+        await Clients.Group(orderId).SendAsync("ReceivePaymentStatus", status);
+    }
+}
+
 }
