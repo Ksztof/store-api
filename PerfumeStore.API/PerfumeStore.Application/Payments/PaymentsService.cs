6bfe60c43446f380e1b7a8c0293e27645705b9cd
Krzysztof Kozłowski <106349197+Ksztof@users.noreply.github.com>
2024-06-17 21:04:15 +0100

    Workingversion (#165)
    * Error and AuthenticationError now store information about error type, returned errors in services are changed according to changed Error classes
    
    * payment errors class has been added
    
    * ResultExtensions has been created and Error class has been upgraded,
    
    * Conflicts are resolved
    
    * Mutliplied files have been fixed

--- PerfumeStore.API/PerfumeStore.Application/Payments/PaymentsService.cs
+++ PerfumeStore.API/PerfumeStore.Application/Payments/PaymentsService.cs
@@ -23,7 +23,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PerfumeStore.Application.Abstractions;
 namespace PerfumeStore.Application.Payments
 {
     public class PaymentsService : IPaymentsService
