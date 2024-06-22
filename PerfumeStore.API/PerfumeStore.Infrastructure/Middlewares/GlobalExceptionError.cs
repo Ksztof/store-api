dd6f883db4d960745a2e20cc5cfd3daad2f22650
Krzysztof Kozłowski <106349197+Ksztof@users.noreply.github.com>
2024-06-22 22:57:43 +0100

    global exception handling middleware has been created (#168)

---
+++ PerfumeStore.API/PerfumeStore.Infrastructure/Middlewares/GlobalExceptionError.cs
@@ -0,0 +1,23 @@
+﻿using PerfumeStore.Application.Abstractions.Result.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfumeStore.Infrastructure.Middlewares
+{
+    internal sealed record GlobalExceptionError
+    {
+        public GlobalExceptionError(string code, string? description, ErrorType errorType)
+        {
+            Code = code;
+            Description = description;
+            Type = errorType;
+        }
+
+        public string Code { get; }
+        public string Description { get; }
+        public ErrorType Type { get; }
+    }
+}
