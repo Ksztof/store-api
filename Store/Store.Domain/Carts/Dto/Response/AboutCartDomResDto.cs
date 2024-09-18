9abe254faec9d6b1a4562f44aef4812255c94e38
krzysztof kozlowski <krzysztofkozlowski1995@gmail.com>
2024-09-18 21:56:11 +0200

    added Dto suffix to AboutCartDomResDto

---
+++ Store/Store.Domain/Carts/Dto/Response/AboutCartDomResDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.Domain.Carts.Dto.Response;
+
+public class AboutCartDomResDto
+{
+    public decimal TotalCartValue { get; set; }
+    public IEnumerable<CheckCartDomRes> AboutProductsInCart { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
