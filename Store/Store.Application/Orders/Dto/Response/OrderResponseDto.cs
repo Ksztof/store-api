6444ce2ebd04754b3c94ec14b9459768ff0c34bf
krzysztof kozlowski <krzysztofkozlowski1995@gmail.com>
2024-09-18 14:26:58 +0200

    Adde suffix dto to product response and orde resposne

---
+++ Store/Store.Application/Orders/Dto/Response/OrderResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using Store.Domain.Carts.Dto.Response;
+
+namespace Store.Application.Orders.Dto.Response;
+
+public class OrderResponseDto
+{
+    public int Id { get; set; }
+    public decimal TotalCartValue { get; set; }
+    public IEnumerable<CheckCartDomRes> AboutProductsInCart { get; set; }
+    public ShippingDetailResponse ShippingDetil { get; set; }
+}
